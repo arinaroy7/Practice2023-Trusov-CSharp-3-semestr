2cb2f946dbcd82965d4132e809b47a06140d8087
arinaroy7 <146703972+arinaroy7@users.noreply.github.com>
2023-11-04 14:32:20 +0400

    Program.cs, User.cs, Order.cs, YourNameSpace.cs
    Добавила новые классы, подключила их в Program.cs. Cделала вывод базы данных - > сумму по заказам пользователей.

---
+++ working with a database/Order.cs
@@ -0,0 +1,9 @@
+namespace YourNamespace {
+    public class Order {
+
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; } 
+        public int UserID { get; set; }
+    }
+}
--- working with a database/User.cs
+++ working with a database/User.cs
@@ -1,57 +1,22 @@
-using Microsoft.Data.Sqlite;
-using Dapper;
-using System;
-
-partial class Program
-{
-    static void Main(string[] args)
+namespace YourNamespace {
+    public class User
     {
-        string connectionString = "Data Source=C:\\Users\\arina\\Desktop\\Учеба\\Учеба 2 курс УдГУ\\Андрей Серг с#\\Базы данных\\people.db";
-
-        using (var connection = new SqliteConnection(connectionString))
-        {
-            connection.Open();
-
-            
-
-        }    
+        public int ID { get; set; } //идентификатор пользователя
+        public string? Login { get; set; }
+        public string? Password { get; set; }
     }
-
-
-    /*static bool CheckLogin(string login, string pass)
-
-    {
-        using (var conn = new SqliteConnection(connectionString))
-        {
-            string sql = "SELECT COUNT(*) FROM Users WHERE Login = @Login AND Password = @Password";
-            return conn.QueryFirstOrDefault<int>(sql, new { Login = login, Password = pass }) > 0;
-        }
-    }
-
-    static long CreateUser(users user)
-    {
-        using (var conn = new SqliteConnection(connectionString))
-        {
-            string sql = "INSERT INTO Users (Login, Password) VALUES (@Login, @Password); SELECT last_insert_rowid()";
-            return conn.QueryFirstOrDefault<long>(sql, user);
-        }
-    }
-
-    static void UpdateUser(User user)
-    {
-        using (var conn = new SqliteConnection(connectionString))
-        {
-            string sql = "UPDATE Users SET Login = @Login, Password = @Password WHERE Id = @Id";
-            conn.Execute(sql, user);
-        }
-    }
-
-    static void DeleteUser(int id)
-    {
-        using (var conn = new SqliteConnection(connectionString))
-        {
-            string sql = "DELETE FROM Users WHERE Id = @Id";
-            conn.Execute(sql, new { Id = id });
-        }
-    }*/
 }
+
+/* Пара за 26 октября
+
+void Trade(int byerid, int sellerid, int itemid) { //функцию вызываем, когда один игрок что-то продает 
+    using var conn = mew SqliteConnection("Data Source=...db");
+
+    int byergold = conn.Query<>
+
+    int cost 
+    //проверяем хватит ли у продавца денег
+    if (byergold < cost) return;
+
+    conn.Execute("UPDATE SET gold=@gold WHERE id=@id", new{}); //
+} */
